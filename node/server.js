6cc0be0873e58a48f71206dfb60c75a2781143c4
Aniol Oliver <anioliver@undertile.net>
2013-07-19 13:25:15 +0200

    md5

--- node/server.js
+++ node/server.js
@@ -1,18 +1,31 @@
 var http = require("http");
 
+var md5 = require("./md5");
+
 function iniciar(route) {
   function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
-    console.log("Petició per "+pathname + " rebuda.");
     
+	if (request.url != '/favicon.ico') {
+	console.log("Petició per "+pathname + " rebuda.");
+    getKeys(pathname);
 
-    route(pathname);
+   // route(pathname);
     
     
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("Tot a punt");
     response.end();
-  }
+  }}
+  
+  function getKeys(obj){
+	  	console.log("calculant el hash de"+obj);
+	    var key;
+	    key=hex_md5(obj);
+	    console.log("el hash es" + key);
+	    return key;
+	    
+	}
 
   http.createServer(onRequest).listen(8080);
   console.log("Servidor Iniciat.");
