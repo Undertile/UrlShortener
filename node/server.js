e854954d9924ae847ad89a6f034fc7823d705c87
Aniol Oliver <anioliver@undertile.net>
2013-07-22 13:43:43 +0200

    anem avançant

--- node/server.js
+++ node/server.js
@@ -2,19 +2,36 @@ var http = require("http");
 
 var md5 = require("./md5");
 
+//var credentials = require ("/etc/UrlShortener/credentials.json");
+
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath("/etc/UrlShortener/credencialsS3.json");
+
+var s3 = new AWS.S3();
+
+
+
 function iniciar(route) {
   function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
     
 	if (request.url != '/favicon.ico') {
 	console.log("Petició per "+pathname + " rebuda.");
-    getKeys(pathname);
+	
+
+	
+	//console.log(s3.listObjects({params: {Bucket:'undertile-urlshort'}}));
+    var shash = getKeys(pathname);
+    createObject(shash, 'http://undertile.com');
+    llistar();
 
    // route(pathname);
     
     
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("Tot a punt");
+    
     response.end();
   }}
   
@@ -25,10 +42,38 @@ function iniciar(route) {
             var key2; 
             key2 = key.substring(0, 8);
 	    console.log("el hash es " + key2);
-	    return key;
+	    return key2;
 	    
 	}
 
+  function createObject(shash,url){
+	  console.log("a punt per crear l'objecte");
+	  var params = {Bucket:'undertile-urlshort',Key:shash,WebsiteRedirectLocation:url, ContentType:'txt/html', CacheControl:'no-cache'};
+
+		s3.putObject(params, function(err, data){
+			if (err) {
+				console.log(err);
+			}
+			else {
+				console.log("Objectes ", data);
+			}
+		});
+	  
+  }
+  
+  function llistar(){
+			var params = {Bucket:'undertile-urlshort'};
+
+			s3.listObjects(params, function(err, data){
+				if (err) {
+					console.log(err);
+				}
+				else {
+					console.log("Objectes ", data);
+				}
+			});}
+	  
+ 
   http.createServer(onRequest).listen(8080);
   console.log("Servidor Iniciat.");
 }
